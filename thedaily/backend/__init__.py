ab3986a3788bc5a8655ae5fb353bcc8018227a1b
VikrantReddy <56089699+VikrantReddy@users.noreply.github.com>
2021-07-16 01:47:26 +0530

    Revert "Backend"

--- thedaily/backend/__init__.py
+++
@@ -1,6 +0,0 @@
-from .scrapers import (
-    get_top_jobs_from_indeed, get_top_news_from_googlenews,
-    get_top_posts_from_subreddit
-)
-
-__all__ = ['get_top_news_from_googlenews', 'get_top_posts_from_subreddit', 'get_top_jobs_from_indeed']

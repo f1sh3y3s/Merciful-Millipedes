718260dc4e62ff4b7e461f9cee6b7d3929e093df
riya <faria.ayir@gmail.com>
2021-07-16 16:02:18 +0600

    update: job post list  from scrapped data

--- sections/job.py
+++ sections/job.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 
 from bs4 import BeautifulSoup
 from prompt_toolkit.buffer import Buffer
@@ -9,30 +10,44 @@ from prompt_toolkit.layout.controls import BufferControl
 from prompt_toolkit.layout.margins import ScrollbarMargin
 from prompt_toolkit.widgets import Frame
 
+sys.path.insert(0, '..')
+from thedaily.backend.scrapers.jobs_scrape import \
+    get_top_jobs_from_indeed  # noqa: E402
+
 
 def get_job_data() -> str:
     """Function for fetching job data"""
-    base_dir = os.path.abspath('')
-    filename = 'sections/job_data.json'
-    abs_file = os.path.join(base_dir, filename)
-    input_file = open(abs_file)
-    json_array = json.load(input_file)
+    job_list = get_top_jobs_from_indeed()
     job_document = ''
-    for item in json_array:
-        name = f'Title: {item["name"]}'
-        company = f'Company: {item["company"]}'
-        description_parser = BeautifulSoup(item['description'], 'html.parser').get_text('\n')
-        description = f'Description: {description_parser}'
-        blanks = ''
-        for _ in range(2):
-            for _ in range(100):
-                blanks += '-'
-            blanks += '\n'
-        job_document += f'{name}\n{company}\n{description}\n{blanks}\n\n'
+
+    if len(job_list) > 0:
+        json_array = job_list
+        for item in json_array:
+            job_document += f'{item}\n{create_blank_lines()}\n\n'
+    else:
+        base_dir = os.path.abspath('')
+        filename = 'sections/job_data.json'
+        abs_file = os.path.join(base_dir, filename)
+        input_file = open(abs_file)
+        json_array = json.load(input_file)
+        for item in json_array:
+            name = f'Title: {item["name"]}'
+            company = f'Company: {item["company"]}'
+            description_parser = BeautifulSoup(item['description'], 'html.parser').get_text('\n')
+            description = f'Description: {description_parser}'
+            job_document += f'{name}\n{company}\n{description}\n{create_blank_lines()}\n\n'
     return job_document
 
 
-# get_job_data()
+def create_blank_lines() -> str:
+    """Function for creating separate lines between job post"""
+    blanks = ''
+    for _ in range(2):
+        for _ in range(97):
+            blanks += '-'
+        blanks += '\n'
+    return blanks
+
 
 def popup_window(title: str, body: Container) -> Frame:
     """Return the layout for a pop-up window."""
@@ -42,7 +57,7 @@ def popup_window(title: str, body: Container) -> Frame:
 job_buffer = Buffer(name='job_window', read_only=True, document=Document(get_job_data(), 0),)
 job_buffer_control = BufferControl(buffer=job_buffer)
 body = Window(content=job_buffer_control, right_margins=[ScrollbarMargin(display_arrows=True)],
-              scroll_offsets=ScrollOffsets(top=2, bottom=2), style='bg:#fefefe fg:#000')
+              scroll_offsets=ScrollOffsets(top=2, bottom=2), style='bg:#fefefe fg:#000', wrap_lines=True)
 
 
 job_layout = popup_window('Top Jobs', body)
--- thedaily/backend/models.py
+++ thedaily/backend/models.py
@@ -28,3 +28,10 @@ class JobPost:
     job_url: str
     description: List[str]
     job_location: str = "remote"
+
+    def __repr__(self) -> str:
+        description_text = ''
+        for data in self.description:
+            description_text += f'{data}\n'
+        return f'Title: {self.job_title}\nComapny: {self.company_name}\
+        \nDescription: {description_text}\nRead More: {self.job_url}\n'

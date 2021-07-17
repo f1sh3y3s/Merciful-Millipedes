64a9d944c481a40612d23f9a96f73bc203549dac
anhsirkkrishna <70256295+anhsirkkrishna@users.noreply.github.com>
2021-07-17 15:25:19 +0530

    Update pong_game_medium.py

--- pong_game_medium.py
+++ pong_game_medium.py
@@ -128,11 +128,11 @@ def main():
 
         # Paddle and ball colision.
         if (ball.xcor() > 330 and ball.xcor() < 340) and\
-        (int(ball.ycor()) in list(range(int(paddle_b.ycor() - 50), int(paddle_b.ycor() + 50)))):
+            (int(ball.ycor()) in list(range(int(paddle_b.ycor() - 50), int(paddle_b.ycor() + 50)))):
             ball.setx(330)
             ball.dx *= -1
         if (ball.xcor() < -330 and ball.xcor() > -340) and\
-        (int(ball.ycor()) in list(range(int(paddle_a.ycor() - 50), int(paddle_a.ycor() + 50)))):
+            (int(ball.ycor()) in list(range(int(paddle_a.ycor() - 50), int(paddle_a.ycor() + 50)))):
             ball.setx(-330)
             ball.dx *= -1
 
